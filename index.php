8da83e6d8c1e3b7041e0349c6e875a83ef83e802
lavillaendterm <85003843+lavillaendterm@users.noreply.github.com>
2021-05-29 19:19:11 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once("index.html");
+include_once("about.html");
+include_once("contact.html");
+include_once("products.html");
+include_once("assets");
+include_once("vendor");
+?>
